fd4dc8ba55884f25abc214b8ba19b2fa15765adf
Shubham Shah <97351922+shubham171002@users.noreply.github.com>
2025-03-07 18:41:59 +0530

    Update loss_functions.py
    Code for MSE updated

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def cross_entropy(y_true, y_pred):
+    if y_true.ndim == 1:
+        y_true = np.eye(y_pred.shape[1])[y_true]
     return -np.sum(y_true * np.log(y_pred + 1e-8)) / y_true.shape[0]
 
 def mean_squared_error(y_true, y_pred):
