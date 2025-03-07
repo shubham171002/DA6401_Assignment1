5e3c302605debde0c5c453f48c7d686715881de2
Shubham Shah <97351922+shubham171002@users.noreply.github.com>
2025-03-07 18:34:11 +0530

    Update nag.py
    Mistakenly written code for different optimizer

--- optimizers/nag.py
+++ optimizers/nag.py
@@ -1,5 +1,7 @@
-def momentum_update(weights, biases, dW, dB, learning_rate, velocity, beta=0.9):
+def nesterov_update(weights, biases, dW, dB, learning_rate, velocity, beta=0.9):
     for i in range(len(weights)):
+        v_prev = velocity[i].copy()
         velocity[i] = beta * velocity[i] + (1 - beta) * dW[i]
-        weights[i] -= learning_rate * velocity[i]
+        weights[i] -= learning_rate * (beta * v_prev + (1 - beta) * dW[i])
         biases[i] -= learning_rate * dB[i]
+        
